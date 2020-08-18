3eb4841704a95abe4eb34da35474af0d5ae64a0a
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2020-08-19 05:11:12 +0800

    message: major update - add: auto reduce the size of pic >2MB (only weibo, other pics >2MB will be discarded) - add: send detailed error message - fixed: send wrong pictured and tail message
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- message.py
+++ message.py
@@ -1,46 +1,92 @@
 import re
+import traceback
 import telegram.ext
+from urllib import request
 from htmlparser import get_md
+from telegramRSSbot import manager
 
 isPic = re.compile(r'<img src="(.+?)"')
+getSize = re.compile(r'^Content-Length: (\d+)$', re.M)
+sizes = ['large', 'mw2048', 'mw1024', 'mw720']
+sizeParser = re.compile(r'(^https?://\w+\.sinaimg\.\S+/)(large|mw2048|mw1024|mw720)(/\w+\.\w+$)')
 
 
-def send(chatid, html, feed_title, url, context):
-    if isPic.search(html):
-        pics = isPic.findall(html)
-        send_media_message(chatid, html, feed_title, url, pics, context)
+def send(chatid, xml, feed_title, url, context):
+    try:
+        send_message(chatid, xml, feed_title, url, context)
+    except Exception as e:
+        traceback.print_exc()
+        # send an error message to manager (if set) or chatid
+        send_message(manager, 'Something went wrong while sending this message. Please check:<br><br>' +
+                     traceback.format_exc(), feed_title, url, context)
+        print()
+        pass
+
+
+def send_message(chatid, xml, feed_title, url, context):
+    if isPic.search(xml):
+        pics = validate_pics(isPic.findall(xml))
+        if pics:
+            send_media_message(chatid, xml, feed_title, url, pics, context)
+            return
+
+    send_text_message(chatid, xml, feed_title, url, False, context)
+
+
+def validate_pic_url(url):  # warning: only design for weibo
+    headers = request.urlopen(url).info()
+    size = getSize.search(str(headers)).group(1)
+    if int(size) > 2090000:  # should be 2097152, but preventatively set to 2090000
+        if sizeParser.search(url):  # is a large weibo pic
+            parsed = sizeParser.search(url).groups()
+            reduced = parsed[0] + sizes[sizes.index(parsed[1]) + 1] + parsed[2]
+            return validate_pic_url(reduced)
+        else:  # TODO: reduce non-weibo pic size
+            return None
     else:
-        send_text_message(chatid, html, feed_title, url, False, context)
+        return url
 
 
-def send_text_message(chatid, html, feed_title, url, is_tail, context):
+def validate_pics(pics):  # reduce pic size to <2MB
+    reduced_pics = []
+    for url in pics:
+        reduced = validate_pic_url(url)
+        if reduced:  # warning: too large non-weibo pic will be discarded
+            reduced_pics.append(reduced)
+    return reduced_pics
+
+
+def send_text_message(chatid, xml, feed_title, url, is_tail, context):
     if not is_tail:
-        text_list = get_md(html, feed_title, url)
+        text_list = get_md(xml, feed_title, url)
     else:
-        text_list = html
+        text_list = xml
 
     number = len(text_list)
-    if number == 1:
-        context.bot.send_message(chatid, text_list[0], parse_mode='MarkdownV2')
-    else:
-        for i in range(number):
-            context.bot.send_message(chatid, rf'\({i + 1 + is_tail}/{number + is_tail}\)' + '\n' + text_list[i]
-                                     , parse_mode='MarkdownV2')
+    head = ''
+
+    for i in range(is_tail, number):
+        if number > 1:
+            head = rf'\({i + 1}/{number}\)' + '\n'
+        context.bot.send_message(chatid, head + text_list[i], parse_mode='MarkdownV2', disable_web_page_preview=True)
 
 
-def send_media_message(chatid, html, feed_title, url, pics, context):
-    text_list = get_md(html, feed_title, url, 1024)
+def send_media_message(chatid, xml, feed_title, url, pics, context):
+    text_list = get_md(xml, feed_title, url, 1024)
     number = len(text_list)
+
+    head = ''
+    if number > 1:
+        head = rf'\(1/{number}\)' + '\n'
 
     if len(pics) == 1:
-        context.bot.send_photo(chatid, pics[0], text_list[0], parse_mode='MarkdownV2')
+        context.bot.send_photo(chatid, pics[0], head + text_list[0], parse_mode='MarkdownV2')
     else:
-        pic_objs = get_pic_objs(pics, text_list[0])
+        pic_objs = get_pic_objs(pics, head + text_list[0])
         context.bot.send_media_group(chatid, pic_objs)
 
     if number > 1:
-        for i in range(1, number):
-            send_text_message(chatid, text_list, feed_title, url, True, context)
+        send_text_message(chatid, text_list, feed_title, url, True, context)
 
 
 def get_pic_objs(pics, caption):

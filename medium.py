bb4704b6cc719ffd692732a5f0eaf8f07d4f2c91
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2021-09-07 00:59:51 +0800

    change sinaimg server if telegram bot api sucks
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- medium.py
+++ medium.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import requests
 import re
 import telegram
@@ -10,7 +12,12 @@ import post
 # getVideo = re.compile(r'<video[^>]*\bsrc="([^"]*)"')
 # getSize = re.compile(r'^Content-Length: (\d+)$', re.M)
 sizes = ['large', 'mw2048', 'mw1024', 'mw720', 'middle']
-sizeParser = re.compile(r'(^https?://\w+\.sinaimg\.\S+/)(large|mw2048|mw1024|mw720|middle)(/\w+\.\w+$)')
+sizeParser = re.compile(r'(?P<domain>^https?://\w+\.sinaimg\.\S+/)'
+                        r'(?P<size>large|mw2048|mw1024|mw720|middle)'
+                        r'(?P<filename>/\w+\.\w+$)')
+serverParser = re.compile(r'(?P<url_prefix>^https?:\/\/[a-zA-Z_-]+)'
+                          r'(?P<server_id>\d)'
+                          r'(?P<url_suffix>\.sinaimg\.\S+$)')
 
 
 class Medium:
@@ -47,25 +54,33 @@ class Medium:
             self.valid = False
             return
 
-        if size > max_size or width + height > 10000:  # too large
-            if sizeParser.search(url):  # is a large weibo pic
-                parsed = sizeParser.search(url).groups()
-                if parsed[1] == sizes[-1]:
-                    print('\t\t- Medium too large, dropped: reduced, but still too large.\n'
-                          f'\t\t\t- {url}')
-                    self.valid = False
-                    return
-                self.url = parsed[0] + sizes[sizes.index(parsed[1]) + 1] + parsed[2]
-                self._validate()
-                return
+        if size <= max_size and height <= 10000:  # valid
+            return
+
+        if not sizeParser.search(url):  # is not a weibo pic
             # TODO: reduce non-weibo pic size
             print('\t\t- Medium too large, dropped: non-weibo medium.\n'
                   f'\t\t\t- {url}')
             self.valid = False
             return
 
+        parsed = sizeParser.search(url).groupdict()
+        if parsed['size'] == sizes[-1]:
+            print('\t\t- Medium too large, dropped: reduced, but still too large.\n'
+                  f'\t\t\t- {url}')
+            self.valid = False
+            return
+        self.url = parsed['domain'] + sizes[sizes.index(parsed['size']) + 1] + parsed['filename']
+        self._validate()
+
     def __bool__(self):
         return self.valid
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.original_url == other.original_url
+
+    def change_sinaimg_server(self):
+        return False
 
 
 class Image(Medium):
@@ -74,6 +89,17 @@ class Image(Medium):
 
     def telegramize(self):
         return telegram.InputMediaPhoto(self.url)
+
+    def change_sinaimg_server(self):
+        if not serverParser.search(self.url):  # is not a weibo pic
+            return False
+
+        parsed = serverParser.search(self.url).groupdict()
+        new_server_id = int(parsed['server_id']) + 1
+        if new_server_id > 4:
+            new_server_id = 1
+        self.url = f"{parsed['url_prefix']}{new_server_id}{parsed['url_suffix']}"
+        return True
 
 
 class Video(Medium):
@@ -89,7 +115,7 @@ def get_medium_info(url):
     session.mount('http://', HTTPAdapter(max_retries=1))
     session.mount('https://', HTTPAdapter(max_retries=1))
 
-    with session.get(url, timeout=(10, 10), proxies=env.requests_proxies, stream=True) as response:
+    with session.get(url, timeout=(5, 5), proxies=env.requests_proxies, stream=True) as response:
         size = int(response.headers['Content-Length']) if 'Content-Length' in response.headers else -1
         content_type = response.headers.get('Content-Type')
         pic_header = response.raw.read(min(256, size))
@@ -108,3 +134,29 @@ def get_medium_info(url):
         height = int(pic_header[pointer + 5:pointer + 7].hex(), 16)
 
     return size, width, height
+
+
+class Media:
+    def __init__(self):
+        self._media: List[Medium] = []
+        self._server_change_count = 0
+
+    def add(self, medium: Medium):
+        if medium in self._media:
+            return
+        self._media.append(medium)
+
+    def invalidate_all(self):
+        any(map(lambda m: m.invalidate(), self._media))
+
+    def get_valid_media(self):
+        return tuple(m for m in self._media if m)
+
+    def get_invalid_link(self):
+        return tuple(m.get_link(only_invalid=True) for m in self._media if not m)
+
+    def change_all_sinaimg_server(self):
+        if self._server_change_count < 3 and sum(map(lambda m: m.change_sinaimg_server(), self._media)):
+            self._server_change_count += 1
+            return True
+        return False

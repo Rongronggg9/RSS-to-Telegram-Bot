c15b29c1daf6c84391f3e975ac09dbbc467a18e6
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2021-09-15 10:15:19 +0800

    use lxml instead of html.parser to parse posts
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- post.py
+++ post.py
@@ -70,10 +70,10 @@ class Post:
         self.retries = 0
         xml = xml.replace('\n', '')
         xml = emojify(xml)
-        self.soup = BeautifulSoup(xml, 'html.parser')
+        self.soup = BeautifulSoup(xml, 'lxml')
         self.media: Media = Media()
         self.text = Text(self._get_item(self.soup))
-        self.title = emojify(BeautifulSoup(title, 'html.parser').get_text(strip=True))
+        self.title = emojify(BeautifulSoup(title, 'lxml').get_text(strip=True))
         self.feed_title = feed_title
         self.link = link
         self.author = author

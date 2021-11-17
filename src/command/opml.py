eb2ced0df9894a306d2a1d57857a86ef955f830c
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2021-11-17 15:24:41 +0800

    optimize the raise of `events.StopPropagation`
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- src/command/opml.py
+++ src/command/opml.py
@@ -15,7 +15,6 @@ async def cmd_import(event: Union[events.NewMessage.Event, Message]):
     await event.respond('请发送需要导入的 OPML 文档',
                         buttons=Button.force_reply())
     # single_use=False, selective=Ture, placeholder='请发送需要导入的 OPML 文档'
-    raise events.StopPropagation
 
 
 @permission_required(only_manager=False)
@@ -23,24 +22,23 @@ async def cmd_export(event: Union[events.NewMessage.Event, Message]):
     opml_file = await inner.export_opml(event.chat_id)
     if opml_file is None:
         await event.respond('无订阅')
-        raise events.StopPropagation
+        return
     await event.respond(file=opml_file,
                         attributes=(types.DocumentAttributeFilename(
                             f"RSStT_export_{datetime.utcnow().strftime('%Y%m%d%H%M%S')}.opml"),))
-    raise events.StopPropagation
 
 
 @permission_required(only_manager=False)
 async def opml_import(event: Union[events.NewMessage.Event, Message]):
     reply_message: Message = await event.get_reply_message()
     if not event.is_private and reply_message.sender_id != env.bot_id:
-        raise events.StopPropagation
+        return
     try:
         opml_file = await event.download_media(file=bytes)
     except Exception as e:
         await event.reply('ERROR: 获取文件失败')
         logger.warning(f'Failed to get opml file from {event.chat_id}: ', exc_info=e)
-        raise events.StopPropagation
+        return
     reply: Message = await event.reply('正在处理中...\n'
                                        '如订阅较多或订阅所在的服务器太慢，将会处理较长时间，请耐心等待')
     logger.info(f'Got an opml file from {event.chat_id}')
@@ -48,9 +46,8 @@ async def opml_import(event: Union[events.NewMessage.Event, Message]):
     opml_d = listparser.parse(opml_file.decode())
     if not opml_d.feeds:
         await reply.edit('ERROR: 解析失败或文档不含订阅')
-        raise events.StopPropagation
+        return
 
     import_result = await inner.subs(event.chat_id, *(feed.url for feed in opml_d.feeds))
     logger.info(f'Imported feed(s) for {event.chat_id}')
     await reply.edit(import_result["msg"], parse_mode='html')
-    raise events.StopPropagation

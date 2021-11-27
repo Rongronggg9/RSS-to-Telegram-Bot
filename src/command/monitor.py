82ab386fa10a33f2b28fd67865f7bbf5bd5aee22
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2021-11-28 05:21:41 +0800

    logging adjustment
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- src/command/monitor.py
+++ src/command/monitor.py
@@ -1,7 +1,7 @@
 import asyncio
 from email.utils import format_datetime
 from json import loads, dumps
-from typing import Optional, Union, MutableMapping
+from typing import Union, MutableMapping, Final
 from telethon.errors.rpcerrorlist import UserIsBlockedError, ChatWriteForbiddenError, UserIdInvalidError
 
 from . import inner
@@ -11,6 +11,47 @@ from src.parsing.post import get_post_from_entry, Post
 from src.web import feed_get
 
 logger = log.getLogger('RSStT.monitor')
+
+NOT_UPDATED: Final = 'not_updated'
+CACHED: Final = 'cached'
+EMPTY: Final = 'empty'
+FAILED: Final = 'failed'
+UPDATED: Final = 'updated'
+
+
+class MonitoringLogs:
+    monitoring_counts = 0
+    not_updated = 0
+    cached = 0
+    empty = 0
+    failed = 0
+    updated = 0
+
+    @classmethod
+    def log(cls, not_updated: int, cached: int, empty: int, failed: int, updated: int):
+        cls.not_updated += not_updated
+        cls.cached += cached
+        cls.empty += empty
+        cls.failed += failed
+        cls.updated += updated
+        logger.debug(f'Finished feeds monitoring task: '
+                     f'updated({updated}), '
+                     f'not updated({not_updated}, including {cached} cached and {empty} empty), '
+                     f'fetch failed({failed})')
+        cls.monitoring_counts += 1
+        if cls.monitoring_counts == 10:
+            cls.print_summary()
+
+    @classmethod
+    def print_summary(cls):
+        logger.info(
+            f'Monitoring tasks summary in last 10 minutes: '
+            f'updated({cls.updated}), '
+            f'not updated({cls.not_updated}, including {cls.cached} cached and {cls.empty} empty), '
+            f'fetch failed({cls.failed})'
+        )
+        cls.not_updated = cls.cached = cls.empty = cls.failed = cls.updated = 0
+        cls.monitoring_counts = 0
 
 
 async def run_monitor_task():
@@ -24,23 +65,36 @@ async def run_monitor_task():
 
     result = await asyncio.gather(*(__monitor(feed) for feed in feeds))
 
-    no_update = 0
-    fail = 0
+    not_updated = 0
+    cached = 0
+    empty = 0
+    failed = 0
     updated = 0
 
     for r in result:
-        if r is None:
-            no_update += 1
-        elif r:
+        if r is NOT_UPDATED:
+            not_updated += 1
+        elif r is CACHED:
+            not_updated += 1
+            cached += 1
+        elif r is EMPTY:
+            not_updated += 1
+            empty += 1
+        elif r is UPDATED:
             updated += 1
-        else:
-            fail += 1
+        elif r is FAILED:
+            failed += 1
 
-    logger.info(f'Finished feeds monitoring task: '
-                f'updated({updated}), not updated({no_update}), fetch failed({fail})')
+    MonitoringLogs.log(not_updated, cached, empty, failed, updated)
 
 
-async def __monitor(feed: db.Feed) -> Optional[bool]:
+async def __monitor(feed: db.Feed) -> str:
+    """
+    Monitor the update of a feed.
+
+    :param feed: the feed object to be monitored
+    :return: monitoring result
+    """
     headers = {
         'If-Modified-Since': format_datetime(feed.updated_at)
     }
@@ -51,17 +105,17 @@ async def __monitor(feed: db.Feed) -> Optional[bool]:
 
     if d['status'] == 304:
         logger.debug(f'Fetched (not updated, cached): {feed.link}')
-        return None
+        return CACHED
 
     rss_d = d['rss_d']
     if rss_d is None:  # error occurred
         feed.error_count += 1
         await feed.save()
-        return False
+        return FAILED
 
     if not rss_d.entries:  # empty
         logger.debug(f'Fetched (empty): {feed.link}')
-        return None
+        return EMPTY
 
     # sequence matters so we cannot use a set
     old_hashes = loads(feed.entry_hashes) if feed.entry_hashes else []
@@ -76,7 +130,7 @@ async def __monitor(feed: db.Feed) -> Optional[bool]:
 
     if not updated_hashes:  # not updated
         logger.debug(f'Fetched (not updated): {feed.link}')
-        return None
+        return NOT_UPDATED
 
     logger.info(f'Updated: {feed.link}')
     length = max(len(rss_d.entries) * 2, 20)
@@ -87,7 +141,7 @@ async def __monitor(feed: db.Feed) -> Optional[bool]:
     await feed.save()
     await asyncio.gather(*(__notify_all(feed, entry) for entry in updated_entries))
 
-    return True
+    return UPDATED
 
 
 async def __notify_all(feed, entry: MutableMapping):

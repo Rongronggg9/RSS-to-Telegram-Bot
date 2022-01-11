6921c2ab2c18eec56e24f817300e64903b05ead5
Rongrong <15956627+Rongronggg9@users.noreply.github.com>
2022-01-11 09:59:18 +0800

    minor bugfixes
    Signed-off-by: Rongrong <15956627+Rongronggg9@users.noreply.github.com>

--- src/parsing/html_text.py
+++ src/parsing/html_text.py
@@ -6,6 +6,8 @@ class Text:
     attr: Optional[str] = None
 
     def __init__(self, content: Union["Text", str, list], param: Optional[str] = None, *_args, **_kwargs):
+        if content is None:
+            content = ''
         self.param = param
         if type(content) is type(self) or type(content) is Text:
             self.content = content.content

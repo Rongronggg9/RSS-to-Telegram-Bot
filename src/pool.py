19990abfbc124c6c8c7921776ce96ee3e380e987
Rongrong <i@rong.moe>
2022-05-27 11:39:26 +0800

    feat(*): env var `NO_MULTIPROCESSING`
    Signed-off-by: Rongrong <i@rong.moe>

--- src/pool.py
+++ src/pool.py
@@ -2,20 +2,17 @@
 Various pools.
 """
 from __future__ import annotations
-from typing import Optional
 
 import os
-from asyncio import AbstractEventLoop
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 from time import sleep
 from signal import signal, SIGINT
 
+from . import env
+
 CPU_COUNT = os.cpu_count()
 AVAIL_CPU_COUNT = len(os.sched_getaffinity(0))
-PROCESS_COUNT = min(AVAIL_CPU_COUNT, 3)
-LOOP: Optional[AbstractEventLoop] = None
-
-del os
+PROCESS_COUNT = 1 if env.NO_MULTIPROCESSING else min(AVAIL_CPU_COUNT, 3)
 
 # Asyncio executor. Either a thread pool or a process pool.
 aioExecutor = (
@@ -30,16 +27,12 @@ async def run_async(func, *args):
     """
     Run a CPU-consuming function asynchronously.
     """
-    return await LOOP.run_in_executor(aioExecutor, func, *args)
+    return await env.loop.run_in_executor(aioExecutor, func, *args)
 
 
-def init(loop: AbstractEventLoop):
-    global LOOP
-
-    if CPU_COUNT > 1:
+def init():
+    if PROCESS_COUNT > 1:
         [aioExecutor.submit(sleep, 0.01 * (i + 1)) for i in range(PROCESS_COUNT - 1)]
-
-    LOOP = loop
 
 
 def shutdown():

55699f5276c8d9a0b5dda1ff12a4de8dc8e4998c
Rongrong <i@rong.moe>
2022-07-31 05:44:35 +0800

    feat(*): add shebang to *.py in project root
    Signed-off-by: Rongrong <i@rong.moe>

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import re
 from functools import partial
 from setuptools import setup, find_packages
--- telegramRSSbot.py
+++ telegramRSSbot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import src
 
 if __name__ == '__main__':

057fe5e192a7d800bd0b2c682784632e60925a42
Rongrong <i@rong.moe>
2022-09-06 14:06:40 +0800

    fix(parsing): malformed HTML causes RecursionError
    this is a workaround for upstream issue
    https://github.com/wilsonzlin/minify-html/issues/86#issuecomment-1237677552
    
    Signed-off-by: Rongrong <i@rong.moe>

--- src/compat.py
+++ src/compat.py
@@ -1,17 +1,25 @@
 """Containing something makes the bot compatible with Python 3.7 ~ 3.10, ThreadPoolExecutor/ProcessPoolExecutor."""
 from __future__ import annotations
-from typing import Callable
 
 import sys
+
+_version_info = sys.version_info
+if _version_info < (3, 7):
+    raise RuntimeError("This bot requires Python 3.7 or later")
+
+from typing import Callable
+
 import functools
 
 from aiohttp import ClientResponse
 from bs4 import BeautifulSoup
 from cachetools.keys import hashkey
+from minify_html import minify as minify_html
 
-_version_info = sys.version_info
-if _version_info < (3, 7):
-    raise RuntimeError("This bot requires Python 3.7 or later")
+try:
+    from minify_html_onepass import minify as minify_html_onepass
+except ImportError:
+    minify_html_onepass = None
 
 import ssl
 from contextlib import AbstractContextManager, AbstractAsyncContextManager, suppress
@@ -93,24 +101,43 @@ def ssl_create_default_context():
     return context
 
 
-def parsing_utils_html_validator_preprocess(html: str) -> str:
+def _parsing_utils_html_validator_minify_preprocess(html: str, drop_sr_only: bool) -> str:
+    # fix malformed HTML first, since minify-html is not so robust
+    # (resulting in RecursionError or unexpected format while html_parser parsing the minified HTML)
+    # https://github.com/wilsonzlin/minify-html/issues/86
+    soup = BeautifulSoup(html, 'lxml')
+    if drop_sr_only:
+        for tag in soup.find_all(attrs={'class': 'sr-only'}):
+            with suppress(ValueError, AttributeError):
+                tag.decompose()
+    html = str(soup)
+    soup.decompose()
+    return html
+
+
+def parsing_utils_html_validator_minify(html: str) -> str:
     contains_sr_only = 'sr-only' in html
+    preprocessed = False
     if (
+            minify_html_onepass is None  # requires minify-html-onepass to workaround upstream issue
+            or
             _version_info[1] < 8  # minify-html >0.6.10 requires Python 3.8+
             or
             contains_sr_only  # clear sr-only first, otherwise minify-html cannot strip spaces around them
     ):
-        # fix malformed HTML first, since minify-html is not so robust
-        # (resulting in RecursionError or unexpected format while html_parser parsing the minified HTML)
-        # https://github.com/wilsonzlin/minify-html/issues/86
-        soup = BeautifulSoup(html, 'lxml')
-        if contains_sr_only:
-            for tag in soup.find_all(attrs={'class': 'sr-only'}):
-                with suppress(ValueError, AttributeError):
-                    tag.decompose()
-        html = str(soup)
-        soup.decompose()
-    return html
+        html = _parsing_utils_html_validator_minify_preprocess(html, contains_sr_only)
+        preprocessed = True
+
+    if minify_html_onepass is not None:
+        try:
+            # workaround for https://github.com/wilsonzlin/minify-html/issues/86#issuecomment-1237677552
+            # minify-html-onepass does not allow invalid closing tags
+            return minify_html_onepass(html)
+        except SyntaxError:
+            if not preprocessed:
+                html = _parsing_utils_html_validator_minify_preprocess(html, contains_sr_only)
+
+    return minify_html(html)
 
 
 def cached_async(cache, key=hashkey):

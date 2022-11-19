271aa83a5f9c8cb62c1e3c082cf59f75a5e1eafe
Rongrong <i@rong.moe>
2022-11-19 23:28:10 +0800

    refactor(setup.py): simplify version parsing
    Signed-off-by: Rongrong <i@rong.moe>

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from functools import partial
 from setuptools import setup, find_packages
 from pathlib import Path
 
-version_info = re.search(r"""__version__ *= *['"]([^'"]+)['"]""", Path("src/version.py").read_text())[1]
-version = {'__version__': version_info}
+PROJ_ROOT = Path(__file__).parent
+
+version = re.search(r"""__version__ *= *['"]([^'"]+)['"]""", (PROJ_ROOT / "src/version.py").read_text())[1]
 
 replacePackagePath = partial(re.compile(r'^src').sub, 'rsstt')
 
@@ -14,7 +15,7 @@ source_packages = find_packages(include=['src', 'src.*'])
 proj_packages = [replacePackagePath(name) for name in source_packages]
 
 setup(
-    version=version['__version__'],
+    version=version,
     packages=proj_packages,
     package_dir={'rsstt': 'src'},
 )

ee1bcdb1080076ba371cedfb07118c875fc84f85
Rongrong <i@rong.moe>
2024-06-30 06:30:52 +0800

    refactor(*): mv __init__.py entrypoint.py
    Signed-off-by: Rongrong <i@rong.moe>

--- src/__main__.py
+++ src/__main__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from . import main
+from .entrypoint import main
 
 if __name__ == '__main__':
     main()
--- telegramRSSbot.py
+++ telegramRSSbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import src
+from src.entrypoint import main
 
 if __name__ == '__main__':
-    src.main()
+    main()

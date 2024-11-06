9ab2500078fe54bbc3b542de3b5b5db2da61da7b
Rongrong <i@rong.moe>
2024-11-07 01:28:20 +0800

    perf(parsing.utils): utilize ranged sets ([x-y]) in re patterns
    Signed-off-by: Rongrong <i@rong.moe>

--- src/parsing/utils.py
+++ src/parsing/utils.py
@@ -15,7 +15,7 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from __future__ import annotations
-from typing import Optional, Sequence, Union, Final, Iterable
+from typing import Optional, Sequence, Union, Final, Iterable, Iterator
 
 import re
 import string
@@ -27,7 +27,7 @@ from emoji import emojize
 from telethon.tl.types import TypeMessageEntity
 from functools import partial
 from urllib.parse import urljoin
-from itertools import chain
+from itertools import chain, count, groupby
 
 from .weibo_emojify_map import EMOJIFY_MAP
 from .. import log
@@ -99,18 +99,65 @@ CHARACTERS_TO_ESCAPE_IN_HASHTAG: Final[str] = ''.join(
     sorted(set(SPACES + INVALID_CHARACTERS + string.punctuation + string.whitespace))
 )
 
+escapeSpecialCharInReSet = partial(
+    re.compile(r'([\\\-\[\]])').sub,  # \-[]
+    r'\\\1',
+)
+
+
+def __merge_chars_into_ranged_set(sorted_chars: str) -> str:
+    monotonic: Iterator[int] = count()
+    groups: Iterator[str] = (
+        ''.join(g)
+        for _, g in groupby(sorted_chars, key=lambda char: ord(char) - next(monotonic))
+    )
+    ranged_set: str = ''.join(
+        f'{escapeSpecialCharInReSet(g[0])}-{escapeSpecialCharInReSet(g[-1])}'
+        # Merging 0~1 chars results in an invalid set, while merging two chars is meaningless.
+        if len(g) > 2
+        else escapeSpecialCharInReSet(g)
+        for g in groups
+    )
+    assert re.fullmatch(rf'[{ranged_set}]+', sorted_chars)
+    return ranged_set
+
+
 # false positive:
 # noinspection RegExpUnnecessaryNonCapturingGroup
 EMOJIFY_RE: Final[re.Pattern] = re.compile(rf'\[(?:{"|".join(re.escape(phrase[1:-1]) for phrase in EMOJIFY_MAP)})]')
-emojifyReSub = partial(EMOJIFY_RE.sub, lambda match: EMOJIFY_MAP[match.group(0)])
+emojifyReSub = partial(
+    EMOJIFY_RE.sub,
+    lambda match: EMOJIFY_MAP[match.group(0)],
+)
 
-replaceInvalidCharacter = partial(re.compile(rf'[{INVALID_CHARACTERS}]').sub, ' ')  # use initially
-replaceSpecialSpace = partial(re.compile(rf'[{SPACES[1:]}]').sub, ' ')  # use carefully
-stripBr = partial(re.compile(r'\s*<br\s*/?\s*>\s*').sub, '<br>')
-stripLineEnd = partial(re.compile(rf'[{SPACES}]+\n').sub, '\n')  # use firstly
-stripNewline = partial(re.compile(r'\n{3,}').sub, '\n\n')  # use secondly
-stripAnySpace = partial(re.compile(r'\s+').sub, ' ')
-escapeHashtag = partial(re.compile(rf'[{CHARACTERS_TO_ESCAPE_IN_HASHTAG}]+').sub, '_')
+replaceInvalidCharacter = partial(
+    re.compile(rf'[{__merge_chars_into_ranged_set(INVALID_CHARACTERS)}]').sub,
+    ' ',
+)  # use initially
+replaceSpecialSpace = partial(
+    re.compile(rf'[{__merge_chars_into_ranged_set(SPACES[1:])}]').sub,
+    ' ',
+)  # use carefully
+stripBr = partial(
+    re.compile(r'\s*<br\s*/?\s*>\s*').sub,
+    '<br>',
+)
+stripLineEnd = partial(
+    re.compile(rf'[{__merge_chars_into_ranged_set(SPACES)}]+\n').sub,
+    '\n',
+)  # use firstly
+stripNewline = partial(
+    re.compile(r'\n{3,}').sub,
+    '\n\n',
+)  # use secondly
+stripAnySpace = partial(
+    re.compile(r'\s+').sub,
+    ' ',
+)
+escapeHashtag = partial(
+    re.compile(rf'[{__merge_chars_into_ranged_set(CHARACTERS_TO_ESCAPE_IN_HASHTAG)}]+').sub,
+    '_',
+)
 isAbsoluteHttpLink = re.compile(r'^https?://').match
 isSmallIcon = re.compile(r'(width|height): ?(([012]?\d|30)(\.\d)?px|([01](\.\d)?|2)r?em)').search
 
